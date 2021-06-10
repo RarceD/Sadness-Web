1e9fdebb96ac4dc26ecbc772449d35e987b681e9
RarceD <akimbo170@gmail.com>
2021-06-10 17:54:56 +0200

    new farewell party joke

--- Sadness_Web/public/js/main.js
+++ Sadness_Web/public/js/main.js
@@ -1,10 +1,29 @@
 console.log("Si miras esto eres un nerd mooooi fuerte");
+var pincture_index = 0;
+var intervalID = setInterval(calculate_sadness, 1000);
+var interval_picture = setInterval(random_images, 4000);
+var image_list =
+  [
+    "1.jpeg",
+    "2.jpg",
+    "3.jpeg",
+    "4.jpeg",
+    "5.jpg",
+    "6.jpeg",
+    "7.jpeg",
+    "8.jpeg",
+    "9.jpeg",
+    "10.jpeg",
+  ]
+
+
 
 function calculate_sadness() {
   //Generate the dates of the arrivals
   var fecha_actual = new Date();
   var fecha_erica = new Date(2020, 4, 2, 11, 30, 30, 30);
-  var fecha_silvia = new Date(2020, 10, 2, 8, 30, 30, 30);
+  //cumpleanos = new Date(1995,11,17,3,24,0); December 17, 1995 03:24:00"
+  var fecha_silvia = new Date(2021, 10, 1, 1, 0, 0, 0);
 
   // console.log(fecha_actual);
   // console.log(fecha_erica);
@@ -24,8 +43,12 @@ function calculate_sadness() {
   var output_erica = Math.round(dias_e) + " dias y " + Math.round(horas_e) + ":" + Math.round(minutos_e) + ":" + Math.round(segundos_e) + " horas";
   var output_silvia = Math.round(dias_s) + " dias " + Math.round(horas_s) + ":" + Math.round(minutos_s) + ":" + Math.round(segundos_s) + " horas";
 
-  document.getElementById("erica").innerHTML = output_erica;
+  // document.getElementById("erica").innerHTML = output_erica;
   document.getElementById("silvia").innerHTML = output_silvia;
 
 }
-var intervalID = setInterval(calculate_sadness, 1000);
+function random_images() {
+  document.getElementById("silvi_wapi").src ="public/img/"+  image_list[pincture_index++];
+  if (pincture_index >= image_list.length)
+    pincture_index = image_list.length - 1;
+}
